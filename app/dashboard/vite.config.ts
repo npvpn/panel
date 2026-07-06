a88a0d084bac1c5d382418facca04adf01d91bf2
JYRAVLENOK <ivasiliev812@gmail.com>
2026-07-06 12:32:08 +0700

    add in locales id of version

--- app/dashboard/vite.config.ts
+++ app/dashboard/vite.config.ts
@@ -4,8 +4,13 @@ import svgr from "vite-plugin-svgr";
 import { visualizer } from "rollup-plugin-visualizer";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const i18nVersion = process.env.VITE_BUILD_ID || Date.now().toString();
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    __I18N_VERSION__: JSON.stringify(i18nVersion),
+  },
   plugins: [
     tsconfigPaths(),
     react({
